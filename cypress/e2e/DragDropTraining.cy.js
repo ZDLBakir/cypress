b86ac80504d9024541e36b57fa437b67a6e63d45
ZDLBakir <worldcreator45@gmail.com>
2026-03-20 12:18:09 -0400

    working on no-plugin way to do drag and drop functionality

--- cypress/e2e/DragDropTraining.cy.js
+++ cypress/e2e/DragDropTraining.cy.js
@@ -8,8 +8,31 @@ describe('Practice Drag and Drop', () => {
         cy.get('#column-b > header').should('have.text', 'A')
 
         //No Plugin method
-        /*cy.get('#column-a').trigger('mousedown', {which: 1})
-        cy.get('#column-a').trigger('mousemove', 250,350)
-       cy.get('#column-a').trigger('mouseup', {force: true})*/
+        /*cy.get('#column-a > header').should('have.text', 'A')
+        cy.get('#column-b > header').should('have.text', 'B')
+        const findRectCenter = $el => {
+            const movingBox = $el[0].getBoundingClientRect()
+            return{
+                x: movingBox.left + movingBox.width / 2,
+                y: movingBox.top + movingBox.height / 2,
+            }
+        }
+        cy.get('#column-a').then($source => {
+            cy.get('#column-b').then($target => {
+                // grabs center of 
+                const locA = findRectCenter($source)
+                const locB = findRectCenter($target)
+
+                cy.wrap($source).trigger('mousedown', {button: 0, clientX: locA.x, clientY: locA.y, force: true})
+                cy.wrap($source).trigger('mousemove', {clientX: locB.x, clientY: locB.y, force: true})
+                cy.wrap($source).trigger('mouseup', {force: true})
+
+            })
+        })
+        cy.get('#column-a > header').should('have.text', 'B')
+        cy.get('#column-b > header').should('have.text', 'A')
+        //cy.get('#column-a').trigger('mousedown', {which: 1})
+        //cy.get('#column-a').trigger('mousemove', 250,350)*/
+       
     })
 })
