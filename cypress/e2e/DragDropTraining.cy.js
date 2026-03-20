c3486b115a8d25a3b41b860755f7172a7dd3a504
ZDLBakir <worldcreator45@gmail.com>
2026-03-20 11:31:19 -0400

    Added Drag and Drop Plugin

--- cypress/e2e/DragDropTraining.cy.js
+++ cypress/e2e/DragDropTraining.cy.js
@@ -1,8 +1,15 @@
 describe('Practice Drag and Drop', () => { 
     it('Drop and Drop test case Pass', () => {
         cy.visit('https://practice.expandtesting.com/drag-and-drop')
-        cy.get('#column-a').trigger('mousedown', {which: 1})
+        cy.get('#column-a').should('have.text', 'A')
+        cy.get('#column-b').should('have.text', 'B')
+        cy.get('#column-a').drag('#column-b')
+        cy.get('#column-a').should('have.text', 'B')
+        cy.get('#column-b').should('have.text', 'A')
+
+        //No Plugin method
+        /*cy.get('#column-a').trigger('mousedown', {which: 1})
         cy.get('#column-a').trigger('mousemove', 250,350)
-       cy.get('#column-a').trigger('mouseup', {force: true})
+       cy.get('#column-a').trigger('mouseup', {force: true})*/
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,5 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//This is required for drag and drop plugin
+require('@4tw/cypress-drag-drop')
