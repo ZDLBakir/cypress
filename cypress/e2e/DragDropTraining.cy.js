d1b76c6c338bf8eaf56c74aa0150c517890ff3a2
ZDLBakir <worldcreator45@gmail.com>
2026-03-20 11:37:07 -0400

    Completed Drag and Drop

--- cypress/e2e/DragDropTraining.cy.js
+++ cypress/e2e/DragDropTraining.cy.js
@@ -1,11 +1,11 @@
 describe('Practice Drag and Drop', () => { 
     it('Drop and Drop test case Pass', () => {
         cy.visit('https://practice.expandtesting.com/drag-and-drop')
-        cy.get('#column-a').should('have.text', 'A')
-        cy.get('#column-b').should('have.text', 'B')
+        cy.get('#column-a > header').should('have.text', 'A')
+        cy.get('#column-b > header').should('have.text', 'B')
         cy.get('#column-a').drag('#column-b')
-        cy.get('#column-a').should('have.text', 'B')
-        cy.get('#column-b').should('have.text', 'A')
+        cy.get('#column-a > header').should('have.text', 'B')
+        cy.get('#column-b > header').should('have.text', 'A')
 
         //No Plugin method
         /*cy.get('#column-a').trigger('mousedown', {which: 1})
